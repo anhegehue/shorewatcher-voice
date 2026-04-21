182a6d0f390e6eef3e8e77a043b8770f2b739f1e
anhegehue <2145293137@qq.com>
2026-04-21 15:59:55 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Service Worker for PWA
+const CACHE_NAME = 'shorewatcher-v1';
+const urlsToCache = ['/', '/index.html', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
